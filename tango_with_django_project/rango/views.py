607c5508069e49d32b163465477e39afc52dad0e
nharsch <nigelharsch@gmail.com>
2015-06-16 21:29:04 -0400

    added media dir and views

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -12,4 +12,4 @@ def index(request):
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.")
+    return render(request, 'rango/about.html')
