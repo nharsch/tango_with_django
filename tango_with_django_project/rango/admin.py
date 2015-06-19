90e4003a68472103e50cf84a4a23420707d4295b
nharsch <nigelharsch@gmail.com>
2015-06-19 14:51:16 -0400

    added slugify to models and admin

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-admin.site.register(Category)
+
+# Add in this class to customize the admin interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+# Update the registration to include this customized interface
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
 
 # Register your models here.
