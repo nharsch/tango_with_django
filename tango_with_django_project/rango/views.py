a8d05cf2e1bffdc4b5f56949ec5f97ea53b9f17c
nharsch <nharsch@Nigels-MacBook-Pro-2.local>
2015-06-22 17:34:56 -0400

    added top 5 pages

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -8,14 +8,11 @@ def index(request):
     # Retrieve the top 5 only = or all if less than 5.
     # Place the list in our context_dict dictionary which will be passed to the template engine.
     category_list = Category.objects.order_by('-likes')[:5] # returns just the first five
-    context_dict = {'categories': category_list}
+    pages_list = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list,
+                    'pages': pages_list}
     
     # Render the response and sent it back!
-    return render(request, 'rango/index.html', context_dict)
-
-# Return a rendered response to send to the client. 
-# We make use of the shortcut function to make our lives easier. 
-# Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context_dict)
 
 def about(request):
