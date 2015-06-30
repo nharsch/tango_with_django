43e691c6e3eae1e077740e07174d3c5411c21a91
nharsch <nharsch@Nigels-MacBook-Pro-2.local>
2015-06-30 14:14:41 -0400

    added bootstrap toolkit, life is much easier

--- tango_with_django_project/rango/forms.py
+++ tango_with_django_project/rango/forms.py
@@ -4,7 +4,7 @@ from rango.models import Page, Category, UserProfile
 
 class CategoryForm(forms.ModelForm):
     name = forms.CharField(max_length=128,
-                           help_text="Please enter the category name."
+                           help_text="Please enter the category name:"
                            )
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
