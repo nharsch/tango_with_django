f4195981e42cfa7a7a06d7ca1f0d8f0d850dcfc5
nharsch <nharsch@Nigels-MacBook-Pro-2.local>
2015-07-28 11:24:24 -0400

    page admin now has list_display fields

--- tango_with_django_project/rango/admin.py
+++ tango_with_django_project/rango/admin.py
@@ -6,8 +6,11 @@ from rango.models import Category, Page, UserProfile
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
+class PageAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'url', 'views')
+
 # Update the registration to include this customized interface
 admin.site.register(Category, CategoryAdmin)
-admin.site.register(Page)
+admin.site.register(Page, PageAdmin)
 admin.site.register(UserProfile)
 # Register your models here.
